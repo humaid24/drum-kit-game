9df3e3496c510181456a2db6c6f8d5111346fe5f
Humaid Ahmed Saeed Abuzinjal <abuzinjal@gmail.com>
2023-06-04 15:25:13 +0800

    firs commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var numOfButtons = document.querySelectorAll(".drum").length;
+
+for (let index = 0; index < numOfButtons; index++) {
+  document
+    .querySelectorAll(".drum")
+    [index].addEventListener("click", function () {
+      var buttonInnerHtml = this.innerHTML;
+
+      makeSound(buttonInnerHtml);
+      buttonAnimation(buttonInnerHtml);
+    });
+}
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
